c8582dc392d0b37bb2bb609ac40e201b80ccd7ad
gerpan <nhaths4701@gmail.com>
2021-02-07 20:09:03 +0700

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const app = require('./server')
 const config = require('./config')
 
-const PORT = config.serverSettings.serverPort
+const PORT = config.serverSettings.serverPort || 1234
 app.listen(PORT, () => {
     console.log(`Server list on port ${PORT}`)
 })
